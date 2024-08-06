feccfaf03147f5ed5e4c68940bd3a47299ddf216
Caesar Gutierrez <karim-100gtz@hotmail.com>
2024-08-05 22:18:20 -0500

    Succesfully play through game and added reset button functionality

--- PegGame/ContentView.swift
+++ PegGame/ContentView.swift
@@ -10,43 +10,63 @@ import CoreData
 
 struct ContentView: View {
     
-    init() 
+    init()
     {
         pegGameManager = PegGameManager()
     }
     
-    var pegGameManager : PegGameManager
+    @ObservedObject var pegGameManager : PegGameManager
     
     var body: some View {
         NavigationView {
-            ZStack
+            VStack(spacing:0)
             {
-                Color.black.ignoresSafeArea()
-                VStack{
-                    ForEach(pegGameManager.PegGame, id: \.self) { pegLane in
-                        HStack{
-                            ForEach(pegLane, id: \.self) { peg in
-                                let pegView = PegView(peg: peg)
-                                pegView
-                                    .onTapGesture {
-                                        pegGameManager.SetSelectedPeg(peg: pegView.peg)
-                                    }
-                            }
+                ZStack{
+                    Color.black
+                    .ignoresSafeArea()
+                    HStack
+                    {
+                        Spacer()
+                        VStack
+                        {
+                            Button(
+                                action: {
+                                    pegGameManager.ResetGame()
+                                }, label: {
+                                    Image(systemName: "arrow.clockwise")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 40, height: 40)
+                                })
+                            .padding()
                         }
                     }
                     
                 }
+                .frame(height: 40)
+                ZStack
+                {
+                    Color.black.ignoresSafeArea()
+                    VStack{
+                        ForEach(pegGameManager.PegGame, id: \.self) { pegLane in
+                            HStack{
+                                ForEach(pegLane, id: \.self) { peg in
+                                    let pegView = PegView(peg: peg)
+                                    pegView
+                                        .onTapGesture {
+                                            pegGameManager.CheckBoard(peg: pegView.peg)
+                                        }
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+                
             }
         }
     }
 }
-
-func RandomColor () -> Color {
-    let colors: [Color] = [.blue, .red, .yellow, .white]
-    let randomIndex = Int.random(in: 0..<colors.count)
-    return colors[randomIndex]
-}
-
 
 // View
 struct PegView: View
@@ -77,10 +97,10 @@ struct PegView: View
             Circle()
                 .fill(peg.isCleared ? .black : peg.circleColor)
                 .frame(width: 50, height: 50)
-                    
+            
         }
     }
-   
+    
 }
 
 
--- PegGame/PegGameManager.swift
+++ PegGame/PegGameManager.swift
@@ -8,10 +8,11 @@
 import Foundation
 import SwiftUI
 
-class PegGameManager
+class PegGameManager:ObservableObject
 {
     @Published var PegGame: [[Peg]]
     @Published var SelectedPeg: Peg?
+    
     
     init()
     {
@@ -32,16 +33,85 @@ class PegGameManager
         peg.isSelected = true
         SelectedPeg = peg
     }
-   
+    func CheckBoard(peg: Peg)
+    {
+        print(peg.Position())
+        
+        if (peg.isCleared)
+        {
+            MoveSelected(peg: peg)
+            return
+        }
+        
+        SetSelectedPeg(peg: peg)
+        
+    }
+    
+    func ResetGame()
+    {
+        PegGame = SetUpTrianglePeg()
+    }
+    
+    func MoveSelected(peg: Peg)
+    {
+        guard let selectedPeg = SelectedPeg else {return}
+        
+        let selectedCol = selectedPeg.col
+        let selectedRow = selectedPeg.row
+        
+        let moveToCol = peg.col
+        let moveToRow = peg.row
+        
+        let col = GetMidDistance(moveToPos: moveToCol, selectedPos: selectedCol)
+        let row = GetMidDistance(moveToPos:  moveToRow, selectedPos: selectedRow)
+        
+        guard let col = col else {return}
+        guard let row = row else {return}
+        
+        let pegToEat = PegGame[row][col]
+        if (pegToEat.isCleared || pegToEat == peg)
+        {
+            return
+        }
+        
+        pegToEat.isCleared = true
+        
+        peg.row = selectedRow
+        peg.col = selectedCol
+        PegGame[selectedRow][selectedCol] = peg
+        
+        selectedPeg.row = moveToRow
+        selectedPeg.col = moveToCol
+        PegGame[moveToRow][moveToCol] = selectedPeg
+    }
+    
+    private func GetMidDistance(moveToPos: Int, selectedPos: Int) -> Int?
+    {
+        let distance = (moveToPos - selectedPos)
+        if (abs(distance) == 0)
+        {
+            return selectedPos
+        }
+        else if (abs(distance) > 3)
+        {
+            // We can't possibly move to that location.
+            return nil
+        }
+        else
+        {
+            return distance < 0 ? selectedPos - 1 : selectedPos + 1
+            
+        }
+    }
 }
 
 
 func SetUpTrianglePeg() -> [[Peg]]
 {
     var pegGame: [[Peg]] = []
-    for row in 0...5 {
+    for row in 0..<5 {
         var pegLane: [Peg] = []
-        for col in 0..<row{
+        for col in 0...row{
             let peg = Peg(row: row,col: col)
             pegLane.append(peg)
             
@@ -49,15 +119,15 @@ func SetUpTrianglePeg() -> [[Peg]]
         pegGame.append(pegLane)
     }
     //FIXME:   Set top peg as cleared as default for now.
-    pegGame[1][0].isCleared = true
-    print(pegGame[1][0].Position())
-
+    pegGame[0][0].isCleared = true
+    
     return pegGame
 }
-
-
-
-
+func RandomColor () -> Color {
+    let colors: [Color] = [.blue, .red, .yellow, .white]
+    let randomIndex = Int.random(in: 0..<colors.count)
+    return colors[randomIndex]
+}
 
 // Peg Object
 class Peg: Identifiable, Hashable, ObservableObject
@@ -83,7 +153,6 @@ class Peg: Identifiable, Hashable, ObservableObject
         id = UUID()
     }
     public func Position() -> String {
-        return "\(col)-\(row)"
+        return "COL:\(col)-ROW:\(row)"
     }
-    
 }
